c068e9a17be7b148934ca1d5af155fb849e2f676
lucassherwin <lksherwin@gmail.com>
2021-03-04 16:37:43 -0500

    added text to each page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
+import Home from './components/Home';
+import Navbar from './components/Navbar';
 import Chat from './components/Chat';
+import Factions from './components/Factions';
+import Wallet from './components/Wallet';
+import History from './components/History';
 
 function App() {
 	return (
-		<div className='App'>
-			<Chat />
+		<div>
+			<Navbar />
+			<Switch>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route exact path='/chat'>
+					<Chat />
+				</Route>
+				<Route exact path='/factions'>
+					<Factions />
+				</Route>
+				<Route exact path='/wallet'>
+					<Wallet />
+				</Route>
+				<Route exact path='/history'>
+					<History />
+				</Route>
+			</Switch>
 		</div>
 	);
 }
---
+++ src/components/Factions.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Factions() {
+	return (
+		<div>
+			<h1>Factions</h1>
+			<p>This is the faction page</p>
+		</div>
+	);
+}
